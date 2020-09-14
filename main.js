7492aaf4a908a45a3edcc16a31bfc4c4cebf9d45
purvjeja <jejapurv@gmail.com>
2020-09-14 12:46:48 +0530

    pro

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// <h1>LOADING...00%</h1>
- document.body.style.backgroundColor = "white";
- setInterval(colorchange, 1170);
-function redirect() {
-  window.open("http://bmipurvjeja.000webhostapp.com/");
-}
+document.body.style.backgroundColor = "white";
+setInterval(colorchange, 1170);
+
 function colorchange(){
   if(document.body.style.backgroundColor=="white"){
       document.body.style.backgroundColor = "black";
@@ -11,17 +8,12 @@ function colorchange(){
       document.querySelector("h2").style.color = "white";
       document.querySelector("p").style.color = "white";
       document.querySelector(".fa-github-square").style.color = "white";
-      document.querySelector("b").style.color = "red";
-      document.querySelector('.nai').style.color= "red";
      }
    else{
      document.body.style.backgroundColor = "white";
      document.querySelector("h1").style.color = "black";
-     document.querySelector("h2 ").style.color = "black";
+     document.querySelector("h2").style.color = "black";
      document.querySelector("p ").style.color = "black";
      document.querySelector(".fa-github-square").style.color = "black";
-     document.querySelector("b").style.color= "blue";
-     document.querySelector(".nai").style.color= "blue";
    }
-
 }
